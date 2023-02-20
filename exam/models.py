7a873e6e11c56f8351484e5fe61e38eec0035b22
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-20 23:48:41 +0600

    localized models to russian

--- exam/models.py
+++ exam/models.py
@@ -4,22 +4,26 @@ from student.models import Student
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=30)
-    max_points = models.PositiveSmallIntegerField()
+    name = models.CharField(max_length=30, verbose_name='Название теста')
+    max_points = models.PositiveSmallIntegerField(verbose_name='Максимальное количество баллов')
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
 
     def __str__(self):
         return f"{self.name} - {self.max_points}"
 
 
 class Question(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    points = models.PositiveSmallIntegerField()
-    name = models.CharField(max_length=500)
-    option_a = models.CharField(max_length=200)
-    option_b = models.CharField(max_length=200)
-    option_c = models.CharField(max_length=200)
-    option_d = models.CharField(max_length=200)
-    option_e = models.CharField(max_length=200)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Тест')
+    points = models.PositiveSmallIntegerField(verbose_name='Количество баллов за правильный ответ')
+    name = models.CharField(max_length=500, verbose_name='Содержание вопроса')
+    option_a = models.CharField(max_length=200, verbose_name='Вариант ответа А')
+    option_b = models.CharField(max_length=200, verbose_name='Вариант ответа B')
+    option_c = models.CharField(max_length=200, verbose_name='Вариант ответа C')
+    option_d = models.CharField(max_length=200, verbose_name='Вариант ответа D')
+    option_e = models.CharField(max_length=200, verbose_name='Вариант ответа E')
 
     cat = (
         ('OptionA', 'OptionA'),
@@ -29,17 +33,25 @@ class Question(models.Model):
         ('OptionE', 'OptionE')
     )
 
-    answer = models.CharField(max_length=200, choices=cat)
+    answer = models.CharField(max_length=200, choices=cat, verbose_name='Правильный ответ')
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
 
     def __str__(self):
         return f"{self.course.name}: {self.name} - correct answer {self.answer}"
 
 
 class Result(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    points = models.PositiveSmallIntegerField()
-    date = models.DateTimeField(auto_now=True)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, verbose_name='Студент')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='Тест')
+    points = models.PositiveSmallIntegerField(verbose_name='Количество баллов')
+    date = models.DateTimeField(auto_now=True, verbose_name='Дата сдачи теста')
+
+    class Meta:
+        verbose_name = 'Результат'
+        verbose_name_plural = 'Результаты'
 
     def __str__(self):
         return f"{self.course.name}: {self.student.get_name} - {self.points} points"
