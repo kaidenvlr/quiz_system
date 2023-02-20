d4c814ce77a6593f47d528017b61a0bd45f0e1d3
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-20 23:51:48 +0600

    registered Student model to adminpanel

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from student.models import Student
+
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['user.first_name', 'user.last_name', 'mobile']
