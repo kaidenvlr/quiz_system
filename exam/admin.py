61a5a37c7fe77c8e5178d59369ffe785c2e8d407
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-20 23:45:35 +0600

    registered models in adminpanel

--- exam/admin.py
+++ exam/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from exam.models import Course, Question, Result
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['name', 'max_points']
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['course.name', 'points', 'name', 'answer']
+
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['student.get_name', 'course.name', 'points', 'date']
