7c58f8f71ba3c694e1fa269d8e89555bf43ce98b
kaidenvlr <jzxcvblessed@gmail.com>
2023-02-21 13:57:22 +0600

    exam views

--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
-# Create your views here.
+
+def is_student(user):
+    return user.groups.filter(name='STUDENT').exists()
+
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect('after-login')
+    return render(request, 'exam/index.html')
+
+
+@user_passes_test(is_student)
+def after_login_view(request):
+    if is_student(request.user):
+        return HttpResponseRedirect('student/student-dashboard')
